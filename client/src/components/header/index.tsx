2430c7fd72d5454b7cabcc14996fa9d7f61c21e5
Ivan Butakov <ivanbutakov000@gmail.com>
2024-03-21 00:14:38 +0500

    add logout ui to header component

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,8 +1,19 @@
-import { LogIn, User, UsersRound } from 'lucide-react'
-import { Link } from 'react-router-dom'
+import { LogIn, LogOut, User, UsersRound } from 'lucide-react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { logout, selectUser } from '../../features/auth/authSlice'
 import styles from './index.module.css'
 
 const Header = () => {
+	const user = useSelector(selectUser)
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+
+	const onLogoutClick = () => {
+		dispatch(logout())
+		localStorage.removeItem('token')
+		navigate('/login')
+	}
 	return (
 		<header className={styles.header}>
 			<div className={styles.headerContent}>
@@ -10,18 +21,24 @@ const Header = () => {
 					<UsersRound /> Сотрудники
 				</Link>
 				<nav className={styles.nav}>
-					<ul className={styles.navList}>
-						<li>
-							<Link to='/register' className={styles.navItem}>
-								<User /> Зарегистрироваться
-							</Link>
-						</li>
-						<li>
-							<Link to='/login' className={styles.navItem}>
-								<LogIn /> Войти
-							</Link>
-						</li>
-					</ul>
+					{!user ? (
+						<ul className={styles.navList}>
+							<li>
+								<Link to='/register' className={styles.navItem}>
+									<User /> Зарегистрироваться
+								</Link>
+							</li>
+							<li>
+								<Link to='/login' className={styles.navItem}>
+									<LogIn /> Войти
+								</Link>
+							</li>
+						</ul>
+					) : (
+						<span className={styles.navItem} onClick={onLogoutClick}>
+							<LogOut /> Выйти
+						</span>
+					)}
 				</nav>
 			</div>
 		</header>
